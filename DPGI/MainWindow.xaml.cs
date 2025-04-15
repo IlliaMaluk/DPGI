970953de400b89a6cd302ba948d7d15f2bb8e9ab
IlliaMaluk <32estabes@gmail.com>
2025-04-15 23:07:14 +0300

    Backend

--- DPGI/MainWindow.xaml.cs
+++ DPGI/MainWindow.xaml.cs
@@ -1,24 +1,67 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DPGI
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            var openBinding = new CommandBinding(ApplicationCommands.Open, Execute_Open, CanExecute_Open);
+            var closeBinding = new CommandBinding(ApplicationCommands.Close, Execute_Close, CanExecute_Close);
+            var saveBinding = new CommandBinding(ApplicationCommands.Save, Execute_Save, CanExecute_Save);
+
+            CommandBindings.Add(openBinding);
+            CommandBindings.Add(closeBinding);
+            CommandBindings.Add(saveBinding);
+        }
+
+        private void CanExecute_Save(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = MainTextBox.Text.Trim().Length > 0;
+        }
+
+        private void Execute_Save(object sender, ExecutedRoutedEventArgs e)
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string filePath = System.IO.Path.Combine(desktopPath, "DPGI.txt");
+
+            File.WriteAllText(filePath, MainTextBox.Text);
+            MessageBox.Show("File saved!");
+        }
+        private void CanExecute_Open(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = string.IsNullOrWhiteSpace(MainTextBox.Text);
+        }
+
+        private void Execute_Open(object sender, ExecutedRoutedEventArgs e)
+        {
+            const string filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            var openDialog = new OpenFileDialog()
+            {
+                InitialDirectory = System.AppDomain.CurrentDomain.BaseDirectory,
+                Filter = filter
+            };
+
+            if (openDialog.ShowDialog() == true)
+            {
+                MainTextBox.Text = File.ReadAllText(openDialog.FileName);
+            }
+        }
+
+        private void CanExecute_Close(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = MainTextBox.Text.Length > 0;
+        }
+
+        private void Execute_Close(object sender, ExecutedRoutedEventArgs e)
+        {
+            MainTextBox.Text = "";
         }
     }
 }
