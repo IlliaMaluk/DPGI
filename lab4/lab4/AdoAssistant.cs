5c7ddeb3cd2aeaf7cb7461fcf073e24d47001bd1
IlliaMaluk <32estabes@gmail.com>
2025-05-13 23:41:50 +0300

    lab4

---
+++ lab4/lab4/AdoAssistant.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using System.Configuration;
+using System.Windows;
+
+public class AdoAssistant
+{
+    private string connectionString = ConfigurationManager.ConnectionStrings["connectionString_ADO"].ConnectionString;
+    private DataTable dt;
+
+    public DataTable TableLoad()
+    {
+        if (dt != null) return dt;
+        dt = new DataTable();
+
+        using (SqlConnection con = new SqlConnection(connectionString))
+        {
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Students", con);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+            try
+            {
+                adapter.Fill(dt);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Помилка підключення до БД");
+            }
+        }
+
+        return dt;
+    }
+
+    public void Insert(int number, string name, string group, string address)
+    {
+        using (SqlConnection con = new SqlConnection(connectionString))
+        {
+            string query = "INSERT INTO Students (RecordBookNumber, FullName, GroupName, Address) VALUES (@Num, @Name, @Group, @Addr)";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@Num", number);
+            cmd.Parameters.AddWithValue("@Name", name);
+            cmd.Parameters.AddWithValue("@Group", group);
+            cmd.Parameters.AddWithValue("@Addr", address);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+        }
+    }
+
+    public void Update(int number, string name, string group, string address)
+    {
+        using (SqlConnection con = new SqlConnection(connectionString))
+        {
+            string query = "UPDATE Students SET FullName=@Name, GroupName=@Group, Address=@Addr WHERE RecordBookNumber=@Num";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@Num", number);
+            cmd.Parameters.AddWithValue("@Name", name);
+            cmd.Parameters.AddWithValue("@Group", group);
+            cmd.Parameters.AddWithValue("@Addr", address);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+        }
+    }
+
+    public void Delete(int number)
+    {
+        using (SqlConnection con = new SqlConnection(connectionString))
+        {
+            string query = "DELETE FROM Students WHERE RecordBookNumber=@Num";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@Num", number);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
---
+++ lab4/lab4/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Data;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Xml.Linq;
+using System;
+
+public partial class MainWindow : Window
+{
+    AdoAssistant db = new AdoAssistant();
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+        list.ItemsSource = db.TableLoad().DefaultView;
+    }
+    private void Create_Click(object sender, RoutedEventArgs e)
+    {
+        db.Insert(int.Parse(tbNum.Text), tbName.Text, tbGroup.Text, tbAddress.Text);
+        list.ItemsSource = db.TableLoad().DefaultView;
+    }
+
+    private void Update_Click(object sender, RoutedEventArgs e)
+    {
+        if (list.SelectedItem is DataRowView row)
+        {
+            int id = (int)row["RecordBookNumber"];
+            db.Update(id, tbName.Text, tbGroup.Text, tbAddress.Text);
+            list.ItemsSource = db.TableLoad().DefaultView;
+        }
+    }
+
+    private void Delete_Click(object sender, RoutedEventArgs e)
+    {
+        if (list.SelectedItem is DataRowView row)
+        {
+            int id = (int)row["RecordBookNumber"];
+            db.Delete(id);
+            list.ItemsSource = db.TableLoad().DefaultView;
+        }
+    }
+}
